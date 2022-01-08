e6bed4413cdf677d97af516c652c547034229922
mvila <mvila@voxelgroup.net>
2022-01-08 16:53:26 +0100

    feat: passes if password is 8 chars

--- PasswordValidation/PasswordValidation/Password.cs
+++ PasswordValidation/PasswordValidation/Password.cs
@@ -2,13 +2,18 @@
 {
     public class Password
     {
+        private string _inputPassword;
+
         public Password(string inputPassword)
         {
-            
+            _inputPassword = inputPassword;
         }
 
         public bool IsValid()
         {
+            if (_inputPassword.Length == 8)
+                return true;
+            
             return false;
         }
     }
