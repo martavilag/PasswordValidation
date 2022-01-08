e9f129f4dc8c4c5b4bc0c1ed381897e3e91d0d7e
mvila <mvila@voxelgroup.net>
2022-01-08 16:39:03 +0100

    test: pass if password is 8 chars

---
+++ PasswordValidation.Tests/PasswordValidatorShould.cs
@@ -0,0 +1,22 @@
+using Xunit;
+
+namespace PasswordValidation.Tests
+{
+    
+    public class PasswordValidatorShould
+    {
+        [Fact]
+        public void FailIfLessThan8Chars()
+        {
+            var pass = new Password("jshuyro");
+            Assert.False(pass.IsValid());
+        }
+
+        [Fact]
+        public void PassIfPasswordIs8Chars()
+        {
+            var pass = new Password("posdkoiu");
+            Assert.True(pass.IsValid());
+        }
+    }
+}
