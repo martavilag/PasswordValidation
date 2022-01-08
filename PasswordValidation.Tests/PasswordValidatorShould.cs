4bb7916203e6f41c95574cedcb9a823851a38b1a
mvila <mvila@voxelgroup.net>
2022-01-08 17:47:13 +0100

    refactor: moved fact tests to theory

--- PasswordValidation.Tests/PasswordValidatorShould.cs
+++ PasswordValidation.Tests/PasswordValidatorShould.cs
@@ -5,32 +5,25 @@ namespace PasswordValidation.Tests
     
     public class PasswordValidatorShould
     {
-        [Fact]
-        public void FailIfLessThan8Chars()
+        
+        [Theory]
+        [InlineData("jshuyro")]
+        [InlineData("posdk3fhdsoiu")]
+        public void FailWhenLess8CharsAndNoNumbers(string givenPass)
         {
-            var pass = new Password("jshuyro");
+            var pass = new Password(givenPass);
             Assert.False(pass.IsValid());
         }
 
-        [Fact]
-        public void PassIfPasswordIs8Chars()
+        [Theory]
+        [InlineData("pos8dk3o")]
+        [InlineData("posdkg3g3dsoiu")]
+        public void PassWhen8CharsOrMoreAnd2OrMoreNums(string givenPass)
         {
-            var pass = new Password("pos8dk3o");
+            var pass = new Password(givenPass);
             Assert.True(pass.IsValid());
         }
 
-        [Fact]
-        public void PassIfPasswordIs8OrMoreChars()
-        {
-            var pass = new Password("posdkg3g3dsoiu");
-            Assert.True(pass.IsValid());
-        }
-
-        [Fact]
-        public void FailIfPasswordDoesntHave2NumbersOrMore()
-        {
-            var pass = new Password("posdk3fhdsoiu");
-            Assert.False(pass.IsValid());
-        }
+       
     }
 }
