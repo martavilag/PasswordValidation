2fa4e6282d411850338cddd0839d913e1c551bbd
mvila <mvila@voxelgroup.net>
2022-01-08 16:19:29 +0100

    chore: project creation and gitignore with .idea

---
+++ PasswordValidation/PasswordValidation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PasswordValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
